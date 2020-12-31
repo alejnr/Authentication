ee458f79ffa044d5141bd1528da0fb5b8ee0ed2d
Alex Menlah <a.menlah@itebodoni.edu.it>
2020-12-31 12:33:16 +0100

    setup server

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+
+
+
+
+app.listen(port, function(){
+    console.log('Server started on port', port);
+})
