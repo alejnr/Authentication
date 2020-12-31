4fa2f81deaee20bde0a697a198c3ef9f396ac072
Alex Menlah <a.menlah@itebodoni.edu.it>
2020-12-31 12:36:02 +0100

    rendering routes and pages

--- app.js
+++ app.js
@@ -10,7 +10,19 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 
+app.get('/', function (req, res) {
+    res.render('home')
+})
 
+
+app.get('/login', function (req, res) {
+    res.render('login')
+})
+
+
+app.get('/register', function (req, res) {
+    res.render('register')
+})
 
 
 app.listen(port, function(){
